381060e1a4c3e5df4046485f1eee9829e60643fb
yaoenqi <yaoenqi@163.com>
2016-12-14 09:26:27 +0800

    Update zhCN.lua

--- KkthnxUI/Locales/zhCN.lua
+++ KkthnxUI/Locales/zhCN.lua
@@ -4,204 +4,204 @@ if (GetLocale() ~= "zhCN") then
 end
 
 -- AFKSpin Localization
-L_AFKSCREEN_NOGUILD = "No Guild"
+L_AFKSCREEN_NOGUILD = "无公会"
 -- Announce Localization
-L_ANNOUNCE_FP_USE = "%s used %s."
-L_ANNOUNCE_INTERRUPTED = INTERRUPTED.." %s's \124cff71d5ff\124Hspell:%d:0\124h[%s]\124h\124r!"
-L_ANNOUNCE_PC_ABORTED = "Pull ABORTED!"
-L_ANNOUNCE_PC_GO = "GO!"
-L_ANNOUNCE_PC_MSG = "Pulling %s in %s.."
-L_ANNOUNCE_SAPPED = "Sapped"
-L_ANNOUNCE_SAPPED_BY = "Sapped by: "
+L_ANNOUNCE_FP_USE = "%s 使用了 %s."
+L_ANNOUNCE_INTERRUPTED = INTERRUPTED.." %s的 \124cff71d5ff\124H法术:%d:0\124h[%s]\124h\124r!"--need test
+L_ANNOUNCE_PC_ABORTED = "停手!"
+L_ANNOUNCE_PC_GO = "上!"
+L_ANNOUNCE_PC_MSG = "Pulling %s in %s.."--need test
+L_ANNOUNCE_SAPPED = "被闷棍!"
+L_ANNOUNCE_SAPPED_BY = "被他闷棍: "
 -- Auras Localization
-L_AURAS_MOVEBUFFS = "Move Buffs"
-L_AURAS_MOVEDEBUFFS = "Move Debuffs"
+L_AURAS_MOVEBUFFS = "移动增益"
+L_AURAS_MOVEDEBUFFS = "移动减益"
 -- Merchant Localization
-L_MERCHANT_NOTENOUGHMONEY = "You don't have enough money to repair!"
-L_MERCHANT_REPAIRCOST = "Your items have been repaired for"
-L_MERCHANT_SOLDTRASH = "Your vendor trash has been sold and you earned"
+L_MERCHANT_NOTENOUGHMONEY = "你没有足够的钱来修理!"
+L_MERCHANT_REPAIRCOST = "你修理物品花了"
+L_MERCHANT_SOLDTRASH = "你售出垃圾获得"
 -- Bindings Localization
-L_BIND_BINDING = "Binding"
-L_BIND_CLEARED = "All keybindings cleared for"
-L_BIND_DISCARD = "All newly set keybindings were discarded."
-L_BIND_INSTRUCT = "Hover, your mouse over any action button, to bind it. Press the escape key or right click to clear the current action button's keybinding."
-L_BIND_KEY = "Key"
-L_BIND_NO_SET = "No bindings set"
-L_BIND_SAVED = "All keybindings have been saved."
+L_BIND_BINDING = "按键绑定"
+L_BIND_CLEARED = "所有按键绑定已清除"
+L_BIND_DISCARD = "所有最近的绑定设置已解除."
+L_BIND_INSTRUCT = "悬停鼠标在任何动作条按钮来绑定它. 按下ESC键或右键点击来清除当前的按键绑定."
+L_BIND_KEY = "键"
+L_BIND_NO_SET = "无绑定设置"
+L_BIND_SAVED = "所有按键绑定已保存."
 -- Chat Localization
-L_CHAT_AFK = "AFK"
-L_CHAT_DND = "DND"
-L_CHAT_GUILD = "G"
-L_CHAT_GUILDRECRUITMENT = "GuildRecruitment"
-L_CHAT_INSTANCE = "I"
-L_CHAT_INSTANCE_LEADER = "IL"
-L_CHAT_LOCALDEFENSE = "LocalDefense"
-L_CHAT_LOOKINGFORGROUP = "LookingForGroup"
-L_CHAT_OFFICER = "O"
-L_CHAT_PARTY = "P"
-L_CHAT_PARTY_LEADER = "PL"
-L_CHAT_RAID = "R"
-L_CHAT_RAID_LEADER = "RL"
-L_CHAT_RAID_WARNING = "RW"
-L_CHAT_PET_BATTLE = "Pet Battle"
+L_CHAT_AFK = "暂离"
+L_CHAT_DND = "勿扰"
+L_CHAT_GUILD = "公会"
+L_CHAT_GUILDRECRUITMENT = "公会招募"
+L_CHAT_INSTANCE = "副本"
+L_CHAT_INSTANCE_LEADER = "副本向导"
+L_CHAT_LOCALDEFENSE = "本地防务"
+L_CHAT_LOOKINGFORGROUP = "查找队伍"
+L_CHAT_OFFICER = "官员"
+L_CHAT_PARTY = "小队"
+L_CHAT_PARTY_LEADER = "队长"
+L_CHAT_RAID = "团队"
+L_CHAT_RAID_LEADER = "团长"
+L_CHAT_RAID_WARNING = "团队警告"
+L_CHAT_PET_BATTLE = "宠物对战"
 -- Configbutton Localization
-L_CONFIGBUTTON_FUNC = "Buttonfunctions:"
-L_CONFIGBUTTON_LEFTCLICK = "Left click:"
-L_CONFIGBUTTON_RIGHTCLICK = "Right click:"
-L_CONFIGBUTTON_MIDDLECLICK = "Middle click:"
-L_CONFIGBUTTON_SHIFTCLICK = "Shift + click:"
-L_CONFIGBUTTON_MOVEUI = "Move UI Elements"
-L_CONFIGBUTTON_RECOUNT = "Show/Hide Recount Frame"
-L_CONFIGBUTTON_SKADA = "Show/Hide Skada Frame"
-L_CONFIGBUTTON_CONFIG = "Show KkthnxUI Configmenu"
-L_CONFIGBUTTON_SPEC = "Show KkthnxUI-Specmenu"
-L_CONFIGBUTTON_SPECMENU = "Specialization selection"
-L_CONFIGBUTTON_SPECERROR = "You are already have this spec activ!"
+L_CONFIGBUTTON_FUNC = "按钮功能:"
+L_CONFIGBUTTON_LEFTCLICK = "左键点击:"
+L_CONFIGBUTTON_RIGHTCLICK = "右键点击:"
+L_CONFIGBUTTON_MIDDLECLICK = "中键点击:"
+L_CONFIGBUTTON_SHIFTCLICK = "Shift+点击:"
+L_CONFIGBUTTON_MOVEUI = "移动UI元素"
+L_CONFIGBUTTON_RECOUNT = "显示/隐藏Recount框体"
+L_CONFIGBUTTON_SKADA = "显示/隐藏Skada框体"
+L_CONFIGBUTTON_CONFIG = "显示KkthnxUI配置菜单"
+L_CONFIGBUTTON_SPEC = "显示KkthnxUI-专精菜单"
+L_CONFIGBUTTON_SPECMENU = "专精的选择"
+L_CONFIGBUTTON_SPECERROR = "你已经启用了这个专精!"
 -- DataBars Localization
-L_DATABARS_ARTIFACT_CLICK = "Click: Opens the artifact overview"
-L_DATABARS_ARTIFACT_REMANING = "|cffe6cc80Remaining: %s|r"
-L_DATABARS_HONOR_LEFTCLICK = "|cffccccccLeft Click: Opens the honor frame|r"
-L_DATABARS_HONOR_RIGHTCLICK = "|cffccccccRight Click: Opens the honor talents frame|r"
+L_DATABARS_ARTIFACT_CLICK = "点击:打开神器"
+L_DATABARS_ARTIFACT_REMANING = "|cffe6cc80剩余: %s|r"
+L_DATABARS_HONOR_LEFTCLICK = "|cffcccccc左键点击:打开荣誉面板|r"
+L_DATABARS_HONOR_RIGHTCLICK = "|cffcccccc右键点击:打开荣誉天赋面板|r"
 -- DataText Localization
-L_DATATEXT_BASESASSAULTED = "Bases Assaulted:"
-L_DATATEXT_BASESDEFENDED = "Bases Defended:"
-L_DATATEXT_CARTS_CONTROLLED = "Carts Controlled:"
-L_DATATEXT_DEMOLISHERSDESTROYED = "Demolishers Destroyed:"
-L_DATATEXT_FLAGSCAPTURED = "Flags Captured:"
-L_DATATEXT_FLAGSRETURNED = "Flags Returned:"
-L_DATATEXT_GATESDESTROYED = "Gates Destroyed:"
-L_DATATEXT_GRAVEYARDSASSAULTED = "Graveyards Assaulted:"
-L_DATATEXT_GRAVEYARDSDEFENDED = "Graveyards Defended:"
-L_DATATEXT_ORB_POSSESSIONS = "Orb Possessions:"
-L_DATATEXT_TOWERSASSAULTED = "Towers Assaulted:"
-L_DATATEXT_TOWERSDEFENDED = "Towers Defended:"
-L_DATATEXT_VICTORY_POINTS = "Victory Points:"
-L_DATATEXT_TOTALMEMORY = "Total Memory Usage:"
-L_DATATEXT_DOWNLOAD = "Download: "
-L_DATATEXT_BANDWIDTH = "Bandwidth: "
+L_DATATEXT_BASESASSAULTED = "突袭了基地:"
+L_DATATEXT_BASESDEFENDED = "防守了基地:"
+L_DATATEXT_CARTS_CONTROLLED = "控制了车子:"
+L_DATATEXT_DEMOLISHERSDESTROYED = "摧毁了投石车:"
+L_DATATEXT_FLAGSCAPTURED = "夺取了旗帜:"
+L_DATATEXT_FLAGSRETURNED = "归还了旗帜:"
+L_DATATEXT_GATESDESTROYED = "摧毁的大门:"
+L_DATATEXT_GRAVEYARDSASSAULTED = "突袭了墓地:"
+L_DATATEXT_GRAVEYARDSDEFENDED = "防守了墓地:"
+L_DATATEXT_ORB_POSSESSIONS = "获得了宝珠:"
+L_DATATEXT_TOWERSASSAULTED = "突袭了塔:"
+L_DATATEXT_TOWERSDEFENDED = "防守了塔:"
+L_DATATEXT_VICTORY_POINTS = "胜利点数:"
+L_DATATEXT_TOTALMEMORY = "内存总占用:"
+L_DATATEXT_DOWNLOAD = "下载: "
+L_DATATEXT_BANDWIDTH = "带宽: "
 L_DATATEXT_FPS = "FPS"
 L_DATATEXT_MS = "MS"
 -- AutoInvite Localization
-L_INVITE_ENABLE = "Autoinvite enabled: "
-L_INVITE_DISABLE = "AutoInvite disabled"
+L_INVITE_ENABLE = "自动邀请已启用: "
+L_INVITE_DISABLE = "自动邀请已禁用"
 -- Info Localization
-L_INFO_DISBAND = "Disbanding group..."
-L_INFO_DUEL = "Declined duel request from "
-L_INFO_PET_DUEL = "Declined pet duel request from "
-L_INFO_INVITE = "Accepted invite from "
-L_INFO_SETTINGS_DBM = "Type /settings dbm, to apply the settings DBM."
-L_INFO_SETTINGS_MSBT = "Type /settings msbt, to apply the settings MSBT."
-L_INFO_SETTINGS_SKADA = "Type /settings skada, to apply the settings Skada."
-L_INFO_SETTINGS_Abu = "Type /settings abu, to apply the settings oUF_Abu."
-L_INFO_SETTINGS_ALL = "Type /settings all, to apply the settings for all modifications."
-L_INFO_NOT_INSTALLED = " is not installed."
-L_INFO_SKIN_DISABLED1 = "Skin for "
-L_INFO_SKIN_DISABLED2 = " is disabled."
+L_INFO_DISBAND = "解散队伍..."
+L_INFO_DUEL = "拒绝了决斗请求来自 "
+L_INFO_PET_DUEL = "拒绝了宠物对战请求来自 "
+L_INFO_INVITE = "接受了邀请来自 "
+L_INFO_SETTINGS_DBM = "输入 /settings dbm, 应用设置到DBM."
+L_INFO_SETTINGS_MSBT = "输入 /settings msbt,应用设置到MSBT."
+L_INFO_SETTINGS_SKADA = "输入 /settings skada,应用设置到Skada."
+L_INFO_SETTINGS_Abu = "输入 /settings abu,应用设置到oUF_Abu."
+L_INFO_SETTINGS_ALL = "输入 /settings all,为所有修改应用设置."
+L_INFO_NOT_INSTALLED = " 未安装."
+L_INFO_SKIN_DISABLED1 = "应用皮肤到 "
+L_INFO_SKIN_DISABLED2 = " 已禁用."
 -- Install Message Localization
-L_INSTALL_COMPLETE = "Installation Complete"
+L_INSTALL_COMPLETE = "安装完成"
 -- Loot Localization
-L_LOOT_ANNOUNCE = "Announce to"
-L_LOOT_CANNOT = "Cannot roll"
-L_LOOT_CHEST = ">> Loot from chest"
-L_LOOT_FISH = "Fishing loot"
-L_LOOT_MONSTER = ">> Loot from "
-L_LOOT_RANDOM = "Random Player"
-L_LOOT_SELF = "Self Loot"
-L_LOOT_TO_GUILD = " Guild"
-L_LOOT_TO_INSTANCE = " Instance"
-L_LOOT_TO_PARTY = " Party"
-L_LOOT_TO_RAID = " Raid"
-L_LOOT_TO_SAY = " Say"
+L_LOOT_ANNOUNCE = "通知到"
+L_LOOT_CANNOT = "不能掷骰"
+L_LOOT_CHEST = ">> 从宝箱中拾取"
+L_LOOT_FISH = "钓鱼拾取"
+L_LOOT_MONSTER = ">> 拾取 "
+L_LOOT_RANDOM = "随机玩家"
+L_LOOT_SELF = "自身拾取"
+L_LOOT_TO_GUILD = " 公会"
+L_LOOT_TO_INSTANCE = " 副本"
+L_LOOT_TO_PARTY = " 小队"
+L_LOOT_TO_RAID = " 团队"
+L_LOOT_TO_SAY = " 说"
 -- Mail Localization
-L_MAIL_COMPLETE = "All done."
-L_MAIL_MESSAGES = "messages"
-L_MAIL_NEED = "Need a mailbox."
-L_MAIL_STOPPED = "Stopped, inventory is full."
-L_MAIL_UNIQUE = "Stopped. Found a unique duplicate item in a bag or the bank."
+L_MAIL_COMPLETE = "全部完成."
+L_MAIL_MESSAGES = "消息"
+L_MAIL_NEED = "需要一个邮箱."
+L_MAIL_STOPPED = "已停止,库存已满."
+L_MAIL_UNIQUE = "已停止.在背包或银行中发现一个重复的唯一物品."
 -- World Map Localization
-L_MAP_FOG = "Fog of War"
+L_MAP_FOG = "战争迷雾"
 -- FarmMode Minimap
-L_MINIMAP_FARMMODE_ON = "Farm mode enabled"
-L_MINIMAP_FARMMODE_OFF = "Farm mode disabled"
+L_MINIMAP_FARMMODE_ON = "Farm模式已启用"
+L_MINIMAP_FARMMODE_OFF = "Farm模式已禁用"
 -- Misc Localization
-L_MISC_COPPERSHORT = "|cffeda55fc|r"
-L_MISC_GOLDSHORT = "|cffffd700g|r"
-L_MISC_REPAIR = "Warning! You need to do a repair of your equipment as soon as possible!"
-L_MISC_SILVERSHORT = "|cffc7c7cfs|r"
-L_MISC_UI_OUTDATED = "KkthnxUI is out of date. You can download the newest version from Curse.com. Get the Curse app and have KkthnxUI automatically updated with the Client!"
-L_MISC_UNDRESS = "Undress"
+L_MISC_COPPERSHORT = "|cffeda55f铜|r"
+L_MISC_GOLDSHORT = "|cffffd700金|r"
+L_MISC_REPAIR = "警告! 你需要尽快修理你的装备!"
+L_MISC_SILVERSHORT = "|cffc7c7cf银|r"
+L_MISC_UI_OUTDATED = "KkthnxUI 已过期.你可以从Curse.com下载最新版本.可以使用Curse客户端自动更新KkthnxUI!"
+L_MISC_UNDRESS = "脱光"
 -- Popup Localization
-L_POPUP_ARMORY = "Armory"
-L_POPUP_BOOSTUI = "|cffff0000WARNING|r |n|nThis will optimize your performance by turning down the graphics and tweaking them. Hit accept only if you are having |cffff0000FPS|r issues!|r"
-L_POPUP_DISBAND_RAID = "Are you sure you want to disband the group?"
-L_POPUP_FIX_ACTIONBARS = "There is something wrong with your action bars. Do you want to reload the UI to fix it?"
-L_POPUP_INSTALLUI = "Thank you for choosing |cff3c9bedKkthnxUI|r! |n|nAccept this installation dialog to apply settings."
-L_POPUP_RELOADUI = "Installation is complete. Please click the 'Accept' button to reload the UI. Enjoy |cff3c9bedKkthnxUI|r. |n|nVisit me at |cff3c9bedwww.github.com/kkthnx|r."
-L_POPUP_RESETUI = "Are you sure you want to reset all settings for |cff3c9bedKkthnxUI|r?"
-L_POPUP_RESOLUTIONCHANGED = "We detected a resolution change on your World of Warcraft client. We HIGHLY RECOMMEND restarting your game. Do you want to proceed?"
-L_POPUP_SETTINGS_ALL = "|cffff0000WARNING|r |n|nThis will apply all the supported addons settings and import them to go with |cff3c9bedKkthnxUI|r. This feature will not do anything if you do not have one of the supported add-ons."
-L_POPUP_SETTINGS_DBM = "We need to change the bar positions of |cff3c9bedDBM|r."
+L_POPUP_ARMORY = "英雄榜"
+L_POPUP_BOOSTUI = "|cffff0000警告|r |n|n此设置将通过调整并降低图形选项以优化性能.仅在你有|cffff0000帧数|r 问题|r时选择此项!"
+L_POPUP_DISBAND_RAID = "你确定你想解散队伍吗?"
+L_POPUP_FIX_ACTIONBARS = "你的动作条有些问题.你想重载UI来处理吗?"
+L_POPUP_INSTALLUI = "感谢选择 |cff3c9bedKkthnxUI|r! |n|n接受这个安装对话框来应用设置."
+L_POPUP_RELOADUI = "安装已完成.请点击 '接受' 按钮来重载UI.体验|cff3c9bedKkthnxUI|r吧! |n|n可访问 |cff3c9bedwww.github.com/kkthnx|r."
+L_POPUP_RESETUI = "你确定你想重置|cff3c9bedKkthnxUI|r吗?"
+L_POPUP_RESOLUTIONCHANGED = "我们检测到您的魔兽世界客户端上分辨率的改动. 我们强烈建议你重启游戏.你想要继续吗？"
+L_POPUP_SETTINGS_ALL = "|cffff0000警告|r |n|n这将适用于所有受支持的插件设置,并将其导入到|cff3c9bedKkthnxUI|r.此功能仅对支持的插件有效."
+L_POPUP_SETTINGS_DBM = "我们需要改变|cff3c9bedDBM|r计时条的位置."
 -- Raid Utility Localization
-L_RAID_UTIL_DISBAND = "Disband Group"
-L_POPUP_DISBAND_RAID = "Are you sure you want to disband the group?"
+L_RAID_UTIL_DISBAND = "解散队伍"
+L_POPUP_DISBAND_RAID = "你确定你想解散这个队伍吗?"
 -- Tooltip Localization
-L_TOOLTIP_ACH_COMPLETE = "Your Status: Completed on "
-L_TOOLTIP_ACH_INCOMPLETE = "Your Status: Incomplete"
-L_TOOLTIP_ACH_STATUS = "Your Status:"
-L_TOOLTIP_ITEM_COUNT = "Item count:"
-L_TOOLTIP_ITEM_ID = "Item ID:"
-L_TOOLTIP_SPELL_ID = "Spell ID:"
+L_TOOLTIP_ACH_COMPLETE = "你的状态: 已完成 "
+L_TOOLTIP_ACH_INCOMPLETE = "你的状态: 未完成"
+L_TOOLTIP_ACH_STATUS = "你的状态:"
+L_TOOLTIP_ITEM_COUNT = "物品计数:"
+L_TOOLTIP_ITEM_ID = "物品ID:"
+L_TOOLTIP_SPELL_ID = "法术ID:"
 -- Tutorial Localization
-L_TUTORIAL_MESSAGE_1 = "For technical support visit https://github.com/Kkthnx."
-L_TUTORIAL_MESSAGE_2 = "You can toggle the microbar by using your right mouse button on the minimap."
-L_TUTORIAL_MESSAGE_3 = "You can set your keybinds quickly by typing /kb."
-L_TUTORIAL_MESSAGE_4 = "The focus unit can be set by typing /focus when you are targeting the unit you want to focus. It is recommended you make a macro to do this."
-L_TUTORIAL_MESSAGE_5 = "You can access copy chat and chat menu functions by mouse over the bottom right corner of chat panel and left click on the button that will appear."
-L_TUTORIAL_MESSAGE_6 = "If you are experiencing issues with KkthnxUI try disabling all your addons except KkthnxUI, remember KkthnxUI is a full UI replacement addon, you cannot run two addons that do the same thing."
-L_TUTORIAL_MESSAGE_7 = "To setup which channels appear in which chat frame, right click the chat tab and go to settings."
-L_TUTORIAL_MESSAGE_8 = "You can use the /resetui command to reset all of your movers. You can also type /moveui and just right click a mover to reset its position."
-L_TUTORIAL_MESSAGE_9 = "To move abilities on the actionbars by default hold shift + drag. You can change the modifier key from the actionbar options menu."
-L_TUTORIAL_MESSAGE_10 = "You can see someones average item level of their gear by enabling the item level for tooltip option"
+L_TUTORIAL_MESSAGE_1 = "技术支持请访问 https://github.com/Kkthnx"
+L_TUTORIAL_MESSAGE_2 = "你可以通过右键点击小地图来打开系统菜单条."
+L_TUTORIAL_MESSAGE_3 = "你可以输入 /kb 来快速设置按键绑定."
+L_TUTORIAL_MESSAGE_4 = "当你选中一个目标时,可以输入 /focus 设置为焦点. 建议做成一个宏."
+L_TUTORIAL_MESSAGE_5 = "你可以通过鼠标划过聊天面板右下角显示聊天菜单按钮,左键点击可以复制聊天内容."
+L_TUTORIAL_MESSAGE_6 = "如果你在使用KkthnxUI时遇到了问题,可以尝试禁用除了KkthnxUI以外的所有插件,记住,KkthnxUI是一个替换整体UI的插件,你不能使用两个插件做同样的事."
+L_TUTORIAL_MESSAGE_7 = "要设置哪个频道出现在哪个聊天框,右键单击聊天选项卡,然后设置."
+L_TUTORIAL_MESSAGE_8 = "你可以使用 /resetui 命令来重置你所有的可移动元素. 也可输入 /moveui 然后右键点击一个可移动元素来重置它的位置."
+L_TUTORIAL_MESSAGE_9 = "默认按住Shift键+拖拽来移动动作条. 你可以从动作条选项菜单中修改组合键."
+L_TUTORIAL_MESSAGE_10 = "你可以通过在工具提示选项中启用物品等级来查看某人的平均物品等级"
 -- Wowhead Link Localization
-L_WATCH_WOWHEAD_LINK = "Wowhead Link"
+L_WATCH_WOWHEAD_LINK = "Wowhead链接"
 -- Welcome Localization
-L_WELCOME_LINE_1 = "Welcome to |cff3c9bedKkthnxUI|r v"
+L_WELCOME_LINE_1 = "欢迎使用 |cff3c9bedKkthnxUI|r v"
 L_WELCOME_LINE_2_1 = ""
-L_WELCOME_LINE_2_2 = "Type /cfg to config interface, or visit www.github.com/kkthnx|r"
+L_WELCOME_LINE_2_2 = "输入/cfg来设置界面,或访问 www.github.com/kkthnx|r"
 L_WELCOME_LINE_2_3 = ""
-L_WELCOME_LINE_2_4 = "Some of your questions can be answered by typing /uihelp"
+L_WELCOME_LINE_2_4 = "输入/uihelp可以解答你的一些问题"
 -- Slash Commands Localization
 L_SLASHCMD_HELP = {
     "",
-    "|cff3c9bedAvailable slash commands:|r",
+    "|cff3c9bed可用的命令:|r",
     "--------------------------",
-    "/rl - Reload interface.",
-    "/rc - Activates a ready check.",
-    "/gm - Opens GM frame.",
-    "/rd - Disband party or raid.",
-    "/toraid - Convert to party or raid.",
-    "/teleport - Teleportation from random dungeon.",
-    "/spec, /ss - Switches between talent spec's.",
-    "/frame - Description is not ready.",
-    "/farmmode - Increase the size of the minimap.",
-    "/moveui - Allows the movement of interface elements.",
-    "/resetui - Resets general settings to default.",
-    "/resetconfig - Resets KkthnxUI_Config settings.",
-    "/settings ADDON_NAME - Applies settings to msbt, dbm, skada, or all addons.",
-    "/pulsecd - Self cooldown pulse test.",
-    "/tt - Whisper target.",
-    "/ainv - Enables automatic invitation.",
-    "/cfg - Opens interface settings.",
-    "/patch - Display Wow patch info.",
+    "/rl - 重载界面.",
+    "/rc - 执行团队检查.",
+    "/gm - 打开GM面板.",
+    "/rd - 解散小队或团队.",
+    "/toraid - 转化为小队或团队.",
+    "/teleport - 从随机地下城传送.",
+    "/spec, /ss - 切换天赋专精.",
+    "/frame - 框体.",
+    "/farmmode - 增加小地图的大小.",
+    "/moveui - 允许界面元素的移动.",
+    "/resetui - 重置综合设置为默认值.",
+    "/resetconfig - 重置 KkthnxUI_Config 的设置.",
+    "/settings ADDON_NAME - 应用设置到 msbt, dbm, skada, 或所有插件.",
+    "/pulsecd - 自身冷却闪烁测试.",
+    "/tt - 密语目标.",
+    "/ainv - 启用自动邀请.",
+    "/cfg - 打开界面设置.",
+    "/patch - 显示魔兽世界版本信息.",
     "",
-    "|cff3c9bedAvailable hidden features:|r",
+    "|cff3c9bed可用的隐藏功能:|r",
     "--------------------------",
-    "Right-click minimap for micromenu.",
-    "Middle mouse click minimap for tracking.",
-    "Left click experience bar opens rep frame.",
-    "Left click artifact bar opens artifact frame.",
-    "Hold alt and obtain player ilvl and spec in tooltip.",
-    "Hold shift to scroll instantly to end or start of chat.",
-    "Copy button to the bottom right side of chat.",
-    "Middle mouse click copy button to /roll.",
+    "鼠标右键点击小地图显示系统菜单.",
+    "鼠标中键点击小地图显示追踪菜单.",
+    "鼠标左键点击经验条打开声望面板.",
+    "鼠标左键点击神器条打开神器面板.",
+    "按住Alt键在工具提示中获取玩家物品等级和专精.",
+    "按住Shift键立即滚动到聊天开始或结尾处.",
+    "复制聊天信息按钮在聊天框右下边.",
+    "鼠标中键点击复制聊天信息按钮则 /roll.",
 }
